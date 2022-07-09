c4e4a9cd8b96a6ef56100419f8c5e39fa02c3835
Enas Sello <enas.awad169$gmail.com>
2022-07-09 12:41:04 +0200

    set up project

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+
+
+function App ()
+{
+  return (
+    <div>
+      <header>
+        <a href=''>amazone</a>
+      </header>
+      <main>
+        
+      </main>
+    </div>
+  );
+}
+
+export default App;
